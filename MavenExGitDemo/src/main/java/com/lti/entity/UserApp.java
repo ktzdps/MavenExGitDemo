5a3bb0d42ef275018b9af19d4f97b6a7c2136127
ktzdps <ktzdps@gmail.com>
2022-07-14 14:19:15 +0530

    added new file to New Dev2Branch

--- MavenExGitDemo/src/main/java/com/lti/entity/UserApp.java
+++ MavenExGitDemo/src/main/java/com/lti/entity/UserApp.java
@@ -3,10 +3,7 @@ package com.lti.entity;
 public class UserApp {
 	
 	public static void main(String[] args) {
-		Calculator  c = new Calculator();
-		
-		int r = c.add(100, 300);
-		System.out.println(r);
+		System.out.println("Hello");
 		
 	}
 
